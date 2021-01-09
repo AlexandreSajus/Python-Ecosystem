2db731216f1fbec854555bcf1e3f9faa8c58a0b7
Alexandre Sajus <asajus47@gmail.com>
2021-01-10 00:00:50 +0100

    1.0

--- agents.py
+++ agents.py
@@ -1,18 +1,50 @@
+"""
+PythonEcosystem by Alexandre Sajus
+
+More information at:
+https://github.com/AlexandreSajus/PythonEcosystem
+"""
+
+# agents.py takes care of managing the behaviour of the animals
+
 from random import randint, random
 from math import sqrt, inf
 from copy import deepcopy
 
 
 def distance(agent1, agent2):
+    """
+    Measures the distance between two agents
+    :param agent1, agent2: an animal, bunny or fox
+    :type agent1, agent2: Object
+    :return: distance
+    :rtype: Float
+    """
     return max(sqrt((agent1.x - agent2.x)**2 + (agent1.y - agent2.y)**2), 0.1)
 
 
 def unitVector(agent1, agent2):
+    """
+    Returns the unit vector from agent1 to agent2
+    :param agent1, agent2: an animal, bunny or fox
+    :type agent1, agent2: Object
+    :return: unit vector (x, y)
+    :rtype: Tuple
+    """
     d = distance(agent1, agent2)
     return ((agent2.x - agent1.x)/d, (agent2.y - agent1.y)/d)
 
 
 def legalMove(move, state):
+    """
+    Checks if the move is possible and is not out of bounds
+    :param move: next potential position for an agent (x, y)
+    :type move: Tuple
+    :param state: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :type state: Array
+    :return: True if the move is legal, False elsewise
+    :rtype: Bool
+    """
     yMax = len(state)
     xMax = len(state[0])
     if move[0] < 0 or move[0] >= xMax:
@@ -23,6 +55,15 @@ def legalMove(move, state):
 
 
 def moveTowards(agent, agentT, state, direction):
+    """
+    Move agent towards agentT. If the move is illegal, move randomly
+    :param agent, agentT: an animal, fox or bunny
+    :type agent, agentT: Object
+    :param state: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :type state: Array
+    :param direction: 1 if agent wants to move towards agentT, -1 if agent wants to run away from agentT
+    :type direction: int
+    """
     u = unitVector(agent, agentT)
     xU = u[0]
     yU = u[1]
@@ -49,6 +90,13 @@ def moveTowards(agent, agentT, state, direction):
 
 
 def randomMovement(agent, state):
+    """
+    Move randomly where it is legal to move
+    :param agent: an animal, fox or bunny
+    :type agent: Object
+    :param state: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :type state: Array
+    """
     r = randint(0, 3)
     x = agent.x
     y = agent.y
@@ -61,6 +109,15 @@ def randomMovement(agent, state):
 
 
 def detectPrey(agent, liveAgents, animal):
+    """
+    Detects if agent can see an instance of type animal in his visibility range
+    :param agent: an animal, fox or bunny
+    :type agent: Object
+    :param: liveAgents, a dictionary with key=id_of_agent and value=agent
+    :type liveAgents: Dict
+    :param: animal: fox or bunny class
+    :type: animal: Class
+    """
     minPrey = None
     minDist = inf
     minKey = None
@@ -75,9 +132,11 @@ def detectPrey(agent, liveAgents, animal):
                     minKey = key
     return minPrey, minKey
 
+# Bunny class, its variables are explained in run.py
+
 
 class bunny:
-    def __init__(self, x, y, speed, visibility, gestChance, gestStatus, gestNumber):
+    def __init__(self, x, y, speed, visibility, gestChance, gestStatus, gestNumber, age):
         self.x = x
         self.y = y
         self.speed = speed
@@ -85,43 +144,109 @@ class bunny:
         self.gestChance = gestChance
         self.gestStatus = gestStatus
         self.gestNumber = gestNumber
+        self.age = age
 
-    def act(self, t, state, liveAgents):
+    # act controls the behavior of the agent at every step of the simulation
+    def act(self, t, state, liveAgents, age_bunny):
+        self.age -= 1  # decrease the age (if age reaches 0, the agent dies)
+        if self.age == 0:  # kill the agent if age reaches O
+            for key in liveAgents:
+                if liveAgents[key] == self:
+                    liveAgents.pop(key, None)
+                    break
+        # the agent can only act on some values of t (time), the frequency of these values are defined by speed
         if t % self.speed == 0:
+            # check for foxes in the area
             minFox, minFKey = detectPrey(self, liveAgents, fox)
-            if minFox != None:
+            if minFox != None:  # if there is a fox, run away
                 moveTowards(self, minFox, state, -1)
-            elif self.gestStatus == 0:
+            elif self.gestStatus == 0:  # if there is no fox and the agent doesn't want to reproduce, move randomly
+                # random chance to want to reproduce next turn
                 self.gestStatus = int(random() < self.gestChance)
                 randomMovement(self, state)
             else:
+                # if the agent wants to reproduce, find another bunny
                 minPrey, minKey = detectPrey(self, liveAgents, bunny)
                 if minPrey != None:
                     moveTowards(self, minPrey, state, 1)
-                    if self.x == minPrey.x and self.y == minPrey.y:
+                    if self.x == minPrey.x and self.y == minPrey.y:  # if a bunny has been found, reproduce
                         self.gestStatus = 0
                         maxKey = 0
-                        for key in liveAgents:
+                        for key in liveAgents:  # find an unassigned key in liveAgents for the newborns
                             if key > maxKey:
                                 maxKey = key
                         for i in range(self.gestNumber):
+                            # the newborns are a copy of the parent
                             liveAgents[maxKey + i + 1] = deepcopy(self)
+                            # reset the age of the newborns
+                            liveAgents[maxKey + i + 1].age = age_bunny
 
-                else:
+                else:  # if no partner found, move randomly
                     randomMovement(self, state)
+
+# Fox class, its variables are explained in run.py
 
 
 class fox:
-    def __init__(self, x, y, speed, visibility):
+    def __init__(self, x, y, speed, visibility, age, huntStatus, hunger, hungerThresMin, hungerThresMax, hungerReward, maxHunger,
+                 gestChance, gestStatus, gestNumber):
         self.x = x
         self.y = y
         self.speed = speed
         self.visibility = visibility
+        self.age = age
+        self.huntStatus = huntStatus
+        self.hunger = hunger
+        self.hungerThresMin = hungerThresMin
+        self.hungerThresMax = hungerThresMax
+        self.hungerReward = hungerReward
+        self.maxHunger = maxHunger
+        self.gestChance = gestChance
+        self.gestStatus = gestStatus
+        self.gestNumber = gestNumber
 
-    def act(self, t, state, liveAgents):
+    # act controls the behavior of the agent at every step of the simulation
+    def act(self, t, state, liveAgents, age_fox):
+        self.age -= 1  # decrease age (if age reaches O, the agent dies)
+        # decrease hunger (if hunger reaches O, the agent dies)
+        self.hunger -= 1
+        # hunger can't go over maxHunger
+        self.hunger = min(self.maxHunger, self.hunger)
+        if self.age == 0 or self.hunger == 0:  # kill the agent in case of starvation or aging
+            for key in liveAgents:
+                if liveAgents[key] == self:
+                    liveAgents.pop(key, None)
+                    break
+        # the agent can only act on some values of t (time), the frequency of these values are defined by speed
         if t % self.speed == 0:
-            minPrey, minKey = detectPrey(self, liveAgents, bunny)
-            if minPrey != None:
-                moveTowards(self, minPrey, state, 1)
-                if self.x == minPrey.x and self.y == minPrey.y:
-                    liveAgents.pop(minKey, None)
+            if self.huntStatus == 0:  # if not hunting
+                if self.hunger <= self.hungerThresMin:  # if hunger goes under thresholdMin, go hunting
+                    self.huntStatus = 1
+                if self.gestStatus == 1:  # if the agent wants to reproduce, find another fox
+                    minPrey, minKey = detectPrey(self, liveAgents, fox)
+                    if minPrey != None:
+                        moveTowards(self, minPrey, state, 1)
+                        if self.x == minPrey.x and self.y == minPrey.y:  # if another fox is found, reproduce
+                            self.gestStatus = 0
+                            maxKey = 0
+                            for key in liveAgents:  # find an unassigned key for the newborns
+                                if key > maxKey:
+                                    maxKey = key
+                            for i in range(self.gestNumber):
+                                # the newborns are copies of the parent
+                                liveAgents[maxKey + i + 1] = deepcopy(self)
+                                # reset the age of the newborns
+                                liveAgents[maxKey + i + 1].age = age_fox
+                else:
+                    if self.gestChance > random():  # random chance to want to reproduce
+                        self.gestStatus = 1
+            else:  # if the agent wants to hunt
+                if self.hunger >= self.hungerThresMax:  # if hunger goes over thresholdMax, stop hunting
+                    self.huntStatus = 0
+                minPrey, minKey = detectPrey(
+                    self, liveAgents, bunny)  # find a prey
+                if minPrey != None:
+                    moveTowards(self, minPrey, state, 1)
+                    if self.x == minPrey.x and self.y == minPrey.y:  # if the agent is on the prey, kill the prey
+                        liveAgents.pop(minKey, None)
+                        self.hunger += self.hungerReward
--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+"""
+PythonEcosystem by Alexandre Sajus
+
+More information at:
+https://github.com/AlexandreSajus/PythonEcosystem
+"""
+
+# run.py takes care of creating the world and animating it
+
 import numpy as np
 from random import randint
 
@@ -8,7 +17,21 @@ import matplotlib.animation as animation
 from agents import bunny, fox
 
 
-def createWorld(h, w, n_bunnies, speed_bunny_min, speed_bunny_max, visibility_bunny, gestChance_bunny, gestStatus_bunny, gestNumber_bunny, n_foxes, speed_fox, visibility_fox):
+def createWorld(h, w, n_bunnies, speed_bunny_min, speed_bunny_max, visibility_bunny, gestChance_bunny,
+                gestStatus_bunny, gestNumber_bunny, age_bunny, n_foxes, speed_fox, visibility_fox, huntStatus_fox, age_fox,
+                hunger_fox, hungerThresMin_fox, hungerThresMax_fox, hungerReward_fox, maxHunger_fox, gestChance_fox,
+                gestStatus_fox, gestNumber_fox):
+    """
+    Creates an initial world by generating agents with their initial parameters on a h*w 2D grid
+    :param h, w: size of the world (height, width)
+    :type h, w: Int
+    :param parameters of the agents: explained down there
+    :type parameters of the agents: Int or Float
+    :return: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :rtype: Array
+    :return: liveAgents, a dictionary with key=id_of_agent and value=agent
+    :rtype: Dict
+    """
     state = np.zeros((h, w))
     liveAgents = {}
     for i in range(1, n_bunnies + 1):
@@ -16,18 +39,28 @@ def createWorld(h, w, n_bunnies, speed_bunny_min, speed_bunny_max, visibility_bu
         y = randint(0, h - 1)
         state[y][x] = i
         liveAgents[i] = bunny(
-            x, y, randint(speed_bunny_min, speed_bunny_max), visibility_bunny, gestChance_bunny, gestStatus_bunny, gestNumber_bunny)
+            x, y, randint(speed_bunny_min, speed_bunny_max), visibility_bunny, gestChance_bunny, gestStatus_bunny, gestNumber_bunny, age_bunny)
 
     for j in range(n_bunnies + 1, n_bunnies + 1 + n_foxes):
         x = randint(0, w - 1)
         y = randint(0, h - 1)
         state[y][x] = j
-        liveAgents[j] = fox(x, y, speed_fox, visibility_fox)
+        liveAgents[j] = fox(x, y, speed_fox, visibility_fox, age_fox, huntStatus_fox,
+                            hunger_fox, hungerThresMin_fox, hungerThresMax_fox, hungerReward_fox, maxHunger_fox, gestChance_fox, gestStatus_fox, gestNumber_fox)
 
     return state, liveAgents
 
 
 def updateState(state, liveAgents):
+    """
+    updates state according to liveAgents
+    :param state: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :type state: Array
+    :param: liveAgents, a dictionary with key=id_of_agent and value=agent
+    :type liveAgents: Dict
+    :return: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :rtype: Array
+    """
     state = np.zeros((len(state), len(state[0])))
     for key in liveAgents:
         agent = liveAgents[key]
@@ -38,15 +71,33 @@ def updateState(state, liveAgents):
 
 
 def step(t, state, liveAgents):
+    """
+    Asks every agent to act according to their act function
+    :param t: time
+    :type t: Int
+    :param state: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :type state: Array
+    :param: liveAgents, a dictionary with key=id_of_agent and value=agent
+    :type liveAgents: Dict
+    :return: state, 2D array of size h*w with 0 if the spot is empty or the id of an agent if an agent is in the spot
+    :rtype: Array
+    """
     for key in liveAgents.copy():
         if key in liveAgents:
             agent = liveAgents[key]
-            agent.act(t, state, liveAgents)
+            agent.act(t, state, liveAgents, age_fox)
     state = updateState(state, liveAgents)
     return state
 
 
 def export(liveAgents):
+    """
+    Exports the coordinates of the agents in lists readable by matplotlib
+    :param: liveAgents, a dictionary with key=id_of_agent and value=agent
+    :type liveAgents: Dict
+    :return: XBunnies, YBunnies, XFoxes, YFoxes, list of the coordinates of the agents
+    :rtype: List
+    """
     XBunnies = []
     YBunnies = []
     XFoxes = []
@@ -63,6 +114,13 @@ def export(liveAgents):
 
 
 def count(liveAgents):
+    """
+    counts living bunnies and foxes and the average bunny speed (for natural selection )
+    :param: liveAgents, a dictionary with key=id_of_agent and value=agent
+    :type liveAgents: Dict
+    :return: liveBunnies, liveFoxes, avgSpeed
+    :rtype: Int or Float
+    """
     liveBunnies = 0
     liveFoxes = 0
     speed = 0
@@ -73,22 +131,40 @@ def count(liveAgents):
             speed += agent.speed
         else:
             liveFoxes += 1
-    return liveBunnies, liveFoxes, speed/liveBunnies
+    return liveBunnies, liveFoxes, speed/max(liveBunnies, 0.1)
 
 
-w = 50
-h = 50
-n_bunnies = 60
-speed_bunny_max = 9
-speed_bunny_min = 4
-visibility_bunny = 10
-gestChance_bunny = 0.004
+# Initialization of the variables
+w = 50  # width of world
+h = 50  # height of world
+n_bunnies = 100  # number of bunnies
+speed_bunny_max = 9  # maximum bunny speed (for natural selection study)
+speed_bunny_min = 2  # minimum bunny speed (for natural selection study)
+visibility_bunny = 3  # vision range of bunnies
+gestChance_bunny = 0.001  # chance to want to reproduce for bunnies
+# reproduction status (0 for don't want to reproduce, 1 elsewise)
 gestStatus_bunny = 0
-gestNumber_bunny = 3
-n_foxes = 8
-speed_fox = 5
-visibility_fox = 50
+gestNumber_bunny = 3  # bunnies created per reproduction
+# bunny age (age decreases over time. If age reaches 0, the agent dies)
+age_bunny = 5000
+n_foxes = 11  # number of initial foxes
+speed_fox = 4  # fox speed
+visibility_fox = 100  # vision range of foxes
+age_fox = 800  # fox age
+huntStatus_fox = 0  # hunting status (0 not on the hunt, 1 elsewise)
+# hunger of foxes (hunger decreases over time, If hunger reaches 0, the agent dies)
+hunger_fox = 250
+# if hunger goes under this threshold, the agent starts hunting
+hungerThresMin_fox = 350
+hungerThresMax_fox = 600  # if hunger goes over this threshold, the agent stops hunting
+hungerReward_fox = 150  # hunger reward per bunny kill
+maxHunger_fox = 700  # hunger max limit
+gestChance_fox = 0.0004  # chance to want to reproduce for foxes
+# reproduction status for bunnies (0 for don't want to reproduce, 1 elsewise)
+gestStatus_fox = 0
+gestNumber_fox = 1  # foxes created per reproduction
 
+# Change the font size for matplotlib
 SIZE = 8
 plt.rc('font', size=SIZE)          # controls default text sizes
 plt.rc('axes', titlesize=SIZE)     # fontsize of the axes title
@@ -98,6 +174,7 @@ plt.rc('ytick', labelsize=SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SIZE)    # legend fontsize
 plt.rc('figure', titlesize=SIZE)
 
+# Setting up the plots
 fig = plt.figure()
 ax1 = plt.subplot(221, title="Ecosystem (blue=bunny; red=fox)",
                   xlabel="x (-)", ylabel="y (-)")
@@ -105,17 +182,23 @@ plt.xlim(0, w)
 plt.ylim(0, h)
 bunnies, = ax1.plot([], [], 'bo', ms=3)
 foxes, = ax1.plot([], [], 'ro', ms=3)
-ax2 = plt.subplot(222, title="Average speed of bunnies over time (red=fox speed)",
+
+
+# Plot to study the evolution of average speed of bunnies over time, for natural selection study
+ax2 = plt.subplot(224, title="Average speed of bunnies over time (red=fox speed)",
                   xlabel="time (-)", ylabel="speed (less is faster) (-)")
-plt.xlim(0, 3000)
-plt.ylim(8, 4)
+plt.xlim(0, 4000)
+plt.ylim(7, 2)
 plt.plot([0, 5000], [speed_fox, speed_fox], color='r')
 speedData, = ax2.plot([], [])
-ax3 = plt.subplot(224, title="Bunny population over time",
+
+
+ax3 = plt.subplot(222, title="Population over time",
                   xlabel="time (-)", ylabel="population (-)")
-plt.xlim(0, 3000)
-plt.ylim(0, 500)
-popData, = ax3.plot([], [])
+plt.xlim(0, 4000)
+plt.ylim(0, 200)
+popBunnyData, = ax3.plot([], [])
+popFoxData, = ax3.plot([], [], color='r')
 
 fig.tight_layout(pad=1.5)
 
@@ -124,37 +207,52 @@ def init():
     """initialize animation"""
     bunnies.set_data([], [])
     foxes.set_data([], [])
+    popBunnyData.set_data([], [])
+    popFoxData.set_data([], [])
     speedData.set_data([], [])
-    popData.set_data([], [])
-    return bunnies, foxes, speedData,
+    return bunnies, foxes, popBunnyData, popFoxData, speedData,
 
 
+# Create a new world
 (state, liveAgents) = createWorld(w, h, n_bunnies,
-                                  speed_bunny_min, speed_bunny_max, visibility_bunny, gestChance_bunny, gestStatus_bunny, gestNumber_bunny, n_foxes, speed_fox, visibility_fox)
-t = 0
+                                  speed_bunny_min, speed_bunny_max, visibility_bunny, gestChance_bunny, gestStatus_bunny,
+                                  gestNumber_bunny, age_bunny, n_foxes, speed_fox, visibility_fox, age_fox, huntStatus_fox,
+                                  hunger_fox, hungerThresMin_fox, hungerThresMax_fox, hungerReward_fox, maxHunger_fox,
+                                  gestChance_fox, gestStatus_fox, gestNumber_fox)
+t = 0  # time
 T = []
+popBunnyList = []
+popFoxList = []
 speedList = []
-popList = []
+
+# Animation function
 
 
 def animate(i):
     global t, state, liveAgents
-    state = step(t, state, liveAgents)
-    t += 1
-    T.append(t)
+    state = step(t, state, liveAgents)  # execute a step
+    t += 1  # increment time
+    T.append(t)  # time list for matplotlib
+
     totalCount = count(liveAgents)
-    speedList.append(totalCount[2])
-    popList.append(totalCount[0])
-    print(t, count(liveAgents))
+    popBunnyList.append(totalCount[0])  # update the number of live bunnies
+    popFoxList.append(totalCount[1])  # update the number of live foxes
+    speedList.append(totalCount[2])  # update the average speed of bunnies
+    # export the positions of the agents for matplotlib
     (Xbunnies, Ybunnies, XFoxes, YFoxes) = export(liveAgents)
+
+    # Set data for animation
     bunnies.set_data(Xbunnies, Ybunnies)
     foxes.set_data(XFoxes, YFoxes)
+    popBunnyData.set_data(T, popBunnyList)
+    popFoxData.set_data(T, popFoxList)
     speedData.set_data(T, speedList)
-    popData.set_data(T, popList)
-    return bunnies, foxes, speedData, popData
+
+    return bunnies, foxes, popBunnyData, popFoxData, speedData,
 
 
+# Animation
 ani = animation.FuncAnimation(fig, animate, frames=600,
-                              interval=10, blit=True, init_func=init)
+                              interval=5, blit=True, init_func=init)
 
 plt.show()
