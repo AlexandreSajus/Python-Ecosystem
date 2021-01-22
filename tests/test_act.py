8149cf80bcbdd60012c79c86a78a1505a2a178f7
SaltyGrandma <nicholasklogan@gmail.com>
2021-01-21 22:45:49 -0500

    performance: Baseline Post `detect_prey` changes
    `act` is the next slowest function
    Baseline: (75.983 seconds)
           1000000    1.207    0.000   75.983    0.000 agents.py:146(act)

---
+++ tests/test_act.py
@@ -0,0 +1,12 @@
+import run
+
+
+def test_bunny_act():
+    # Test
+    l_a = run.liveAgents.copy()
+    l_a[1].act(1, run.state, l_a, run.age_fox)
+
+
+if __name__ == "__main__":
+    for i in range(1_000_000):
+        test_bunny_act()
