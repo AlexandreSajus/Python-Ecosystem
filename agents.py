ce5ea5cea0ef91b7a573f386f5d05b3c5361b8e3
SaltyGrandma <nicholasklogan@gmail.com>
2021-01-21 20:40:14 -0500

    pythonic: python uses snake_case and not camelCase for naming

--- agents.py
+++ agents.py
@@ -23,7 +23,7 @@ def distance(agent1, agent2):
     return max(sqrt((agent1.x - agent2.x)**2 + (agent1.y - agent2.y)**2), 0.1)
 
 
-def unitVector(agent1, agent2):
+def unit_vector(agent1, agent2):
     """
     Returns the unit vector from agent1 to agent2
     :param agent1, agent2: an animal, bunny or fox
@@ -35,7 +35,7 @@ def unitVector(agent1, agent2):
     return ((agent2.x - agent1.x)/d, (agent2.y - agent1.y)/d)
 
 
-def legalMove(move, state):
+def legal_move(move, state):
     """
     Checks if the move is possible and is not out of bounds
     :param move: next potential position for an agent (x, y)
@@ -54,7 +54,7 @@ def legalMove(move, state):
     return True
 
 
-def moveTowards(agent, agentT, state, direction):
+def move_towards(agent, agentT, state, direction):
     """
     Move agent towards agentT. If the move is illegal, move randomly
     :param agent, agentT: an animal, fox or bunny
@@ -64,7 +64,7 @@ def moveTowards(agent, agentT, state, direction):
     :param direction: 1 if agent wants to move towards agentT, -1 if agent wants to run away from agentT
     :type direction: int
     """
-    u = unitVector(agent, agentT)
+    u = unit_vector(agent, agentT)
     xU = u[0]
     yU = u[1]
     if abs(xU) >= abs(yU):
@@ -73,23 +73,23 @@ def moveTowards(agent, agentT, state, direction):
         else:
             xU = -1*direction
         move = (agent.x + xU, agent.y)
-        if legalMove(move, state):
+        if legal_move(move, state):
             (agent.x, agent.y) = move
         else:
-            randomMovement(agent, state)
+            random_movement(agent, state)
     else:
         if yU > 0:
             yU = 1*direction
         else:
             yU = -1*direction
         move = (agent.x, agent.y + yU)
-        if legalMove(move, state):
+        if legal_move(move, state):
             (agent.x, agent.y) = move
         else:
-            randomMovement(agent, state)
+            random_movement(agent, state)
 
 
-def randomMovement(agent, state):
+def random_movement(agent, state):
     """
     Move randomly where it is legal to move
     :param agent: an animal, fox or bunny
@@ -102,13 +102,13 @@ def randomMovement(agent, state):
     y = agent.y
     moves = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
     move = moves[r]
-    if legalMove(move, state):
+    if legal_move(move, state):
         (agent.x, agent.y) = move
     else:
-        randomMovement(agent, state)
+        random_movement(agent, state)
 
 
-def detectPrey(agent, liveAgents, animal):
+def detect_prey(agent, liveAgents, animal):
     """
     Detects if agent can see an instance of type animal in his visibility range
     :param agent: an animal, fox or bunny
@@ -157,18 +157,18 @@ class Bunny:
         # the agent can only act on some values of t (time), the frequency of these values are defined by speed
         if t % self.speed == 0:
             # check for foxes in the area
-            minFox, minFKey = detectPrey(self, liveAgents, Fox)
+            minFox, minFKey = detect_prey(self, liveAgents, Fox)
             if minFox != None:  # if there is a fox, run away
-                moveTowards(self, minFox, state, -1)
+                move_towards(self, minFox, state, -1)
             elif self.gestStatus == 0:  # if there is no fox and the agent doesn't want to reproduce, move randomly
                 # random chance to want to reproduce next turn
                 self.gestStatus = int(random() < self.gestChance)
-                randomMovement(self, state)
+                random_movement(self, state)
             else:
                 # if the agent wants to reproduce, find another bunny
-                minPrey, minKey = detectPrey(self, liveAgents, Bunny)
+                minPrey, minKey = detect_prey(self, liveAgents, Bunny)
                 if minPrey != None:
-                    moveTowards(self, minPrey, state, 1)
+                    move_towards(self, minPrey, state, 1)
                     if self.x == minPrey.x and self.y == minPrey.y:  # if a bunny has been found, reproduce
                         self.gestStatus = 0
                         maxKey = 0
@@ -182,7 +182,7 @@ class Bunny:
                             liveAgents[maxKey + i + 1].age = age_bunny
 
                 else:  # if no partner found, move randomly
-                    randomMovement(self, state)
+                    random_movement(self, state)
 
 # Fox class, its variables are explained in run.py
 
@@ -223,9 +223,9 @@ class Fox:
                 if self.hunger <= self.hungerThresMin:  # if hunger goes under thresholdMin, go hunting
                     self.huntStatus = 1
                 if self.gestStatus == 1:  # if the agent wants to reproduce, find another fox
-                    minPrey, minKey = detectPrey(self, liveAgents, Fox)
+                    minPrey, minKey = detect_prey(self, liveAgents, Fox)
                     if minPrey != None:
-                        moveTowards(self, minPrey, state, 1)
+                        move_towards(self, minPrey, state, 1)
                         if self.x == minPrey.x and self.y == minPrey.y:  # if another fox is found, reproduce
                             self.gestStatus = 0
                             maxKey = 0
@@ -243,10 +243,10 @@ class Fox:
             else:  # if the agent wants to hunt
                 if self.hunger >= self.hungerThresMax:  # if hunger goes over thresholdMax, stop hunting
                     self.huntStatus = 0
-                minPrey, minKey = detectPrey(
+                minPrey, minKey = detect_prey(
                     self, liveAgents, Bunny)  # find a prey
                 if minPrey != None:
-                    moveTowards(self, minPrey, state, 1)
+                    move_towards(self, minPrey, state, 1)
                     if self.x == minPrey.x and self.y == minPrey.y:  # if the agent is on the prey, kill the prey
                         liveAgents.pop(minKey, None)
                         self.hunger += self.hungerReward
